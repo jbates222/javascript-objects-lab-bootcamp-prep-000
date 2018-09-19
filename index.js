8e4fe391b9f40c8ad93dbacf336d76542e35229d
Jackson Bates <jkbates222@gmail.com>
2018-09-19 01:02:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var recipes = {};
 var key;
 var value;
-function destructivelyDeleteFromObjectByKey(recipes, key) {
-  delete recipes.key;
-  
-}
+
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {[key]: value});
 }
@@ -16,4 +13,8 @@ function deleteFromObjectByKey(recipes, key) {
   var obj = recipes;
   delete obj.key
   return key
+}
+function destructivelyDeleteFromObjectByKey(recipes, key) {
+  delete recipes.key;
+  
 }
