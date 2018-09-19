571e41f979799e09608c38e6fceb2521b986c57d
Jackson Bates <jkbates222@gmail.com>
2018-09-19 00:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 var key;
 var value;
-recipes[key] = value;
+
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {[key]: value});
 }
